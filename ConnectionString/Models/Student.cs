2623d19f687dec1ca858981d79425ad16e9f7019
Pratikshya <daspratikshya8@gmail.com>
2024-07-04 13:38:41 +0530

    Add project files.

---
+++ ConnectionString/Models/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ConnectionString.Models
+{
+    public class Student
+    {
+        public string StuId { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+        public string Duration { get; set; }
+
+        public SqlConnection connection()
+        {
+            var connectionstring = ConfigurationManager.ConnectionStrings["nareshitconnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(connectionstring);
+            return con;
+        }
+        public List<Student> GetStudentsDetails() 
+        {
+            var con = connection();
+            List<Student>StudList = new List<Student>();
+            var query = "select * from StudentInfo";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach(DataRow dr in dt.Rows)
+            {
+                StudList.Add(
+                    new Student
+                    {
+                        StuId = Convert.ToString(dr["StuId"]),
+                        Name = Convert.ToString(dr["Name"]),
+                        Course = Convert.ToString(dr["Course"]),
+                        Duration = Convert.ToString(dr["Duration"])
+                    });
+            }
+            return StudList;
+        }
+    }
+}
